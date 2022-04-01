69c229a619734cdcbe3ae12eda5091ce09ba5abb
AmarachiOkereke <90873234+AmarachiOkereke@users.noreply.github.com>
2022-04-01 22:10:59 +0100

    BabyName Picker Completed Exercise 1 & 2

---
+++ baby-name-picker/src/RenderNames.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RenderNames = (props) => {
+    return props.names.sort((a, b) => a.name.localeCompare(b.name)).map((babyName, i) => {
+        return (
+            <p key={i} className={babyName.sex} >
+                {babyName.name}
+           </p>
+       )
+   }) 
+}
+export default RenderNames;
